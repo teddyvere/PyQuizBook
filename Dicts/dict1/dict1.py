3a81056a1a781ff4db70c0e5f4927d325107f7d9
Teddy Archibald <107772813+teddyvere@users.noreply.github.com>
2024-07-24 15:50:39 -0400

    fin

--- Dicts/dict1/dict1.py
+++ Dicts/dict1/dict1.py
@@ -2,50 +2,58 @@ def create_dict_from_lists(keys, values):
     """
     This function returns a dict made from two lists.
     """
-    pass  # implement me
+    if len(keys) != len(values):
+        raise ValueError("List length must be equal")
+    
+    return dict(zip(keys, values)) # implement me
 
 def merge_two_dicts(d1, d2):
     """
     Merge two Python dictionaries into one
     """
-    pass  # implement me
+    merge = d1.copy()
+    merge.update(d2)
+    return merge
 
 def init_dict_with_values(lst, d1):
     """
     Create a dict with default values for each key listed.
 
     """
-    #
-    pass  # implement me
-
+    my_dict = {}
+    for key in lst:
+        my_dict[key] = d1
+    return my_dict
+    
+    
 def extract_keys_to_dict(datadict, keylist):
     """
     Create a dictionary by extracting the keylist from a given dictionary
     """
-    #
-    pass  # implement me
+    
+    return {key: datadict[key] for key in keylist if key in datadict}
 
 def delete_keys_from_dict(datadict, keylist):
     """
     Delete a list of keys from a dictionary
     """
-    pass
+    return {k: v for k, v in datadict.items() if k not in keylist}
 
 def check_dict_for_key(datadict, key):
     """
     Check if a value exists in a dictionary
     (NO FOR loops!)
     """
-    pass
+    return key in datadict.values()
 
 def get_key_of_min_value(ddd):
     """
     Get the key of the minimum value from a dictionary
     """
-    pass
+    return min(ddd, key = ddd.get)
 
 def get_key_of_max_value(ddd):
     """
     Get the key of the maximum value from a dictionary
     """
-    pass
+    return max(ddd, key = ddd.get)
--- Lists/list1/list1.py
+++ Lists/list1/list1.py
@@ -1,3 +1,4 @@
+from cmath import sqrt
 from xml.dom.expatbuilder import parseString
 
 
@@ -6,61 +7,59 @@ from xml.dom.expatbuilder import parseString
 def create_list_from_tuple(t):
     """
     This function takes a tuple of elements and returns a list containing those elements of the tuple.
-    """
-    pass  # implement me
+    """ 
+    return list(t) # implement me
     
 def drop_last(lst):
     """
     This function takes a list and returns a list with the last item removed.
     """
-    pass  # implement me
+    return lst[:-1]  # implement me
 
 
 def drop_first_two(lst):
     """
     This function takes a list and returns a list with the first two items removed.
     """
-    pass  # implement me
+    return lst[2:]
 
 def drop_mangle(lst):
     """
     This function takes a list and returns a list with the first two items AND last item removed.
     """
-    pass  # implement me
+    return lst[2: -1]
 
 def add_item_front(lst, a):
     """
     This function takes a list and an item,
     returning the list with the item prepended to the list
     """
-    pass  # implement me
+    return [a] + lst
 
 def add_item_end(lst, a):
     """
     This function takes a list and an item,
     returning the list with the item appended to the list
     """
-    pass  # implement me
-
+    return lst + [a]
 def add_list_to_list(lsta, lstb):
     """
     This function takes two lists and appends one to the other,
     returning a list
     """
-    pass  # implement me
+    return lsta + lstb  # implement me
 
 def list_and_list_to_tuple(lsta, lstb):
     """
     This function takes two lists and returns a tuple containing the two lists
     """
-    pass # implement me
+    return (lsta, lstb)
 
 def list_and_list_to_list(lsta, lstb):
     """
     This function takes two lists and returns a list containing the two lists
     """
-    pass # implement me
-
+    return [lsta, lstb]
 ##
 ##
 ##
@@ -69,77 +68,79 @@ def list_from_range(n):
     """
     This function returns list with 0..n as integers in a list
     """
-    pass # implement me
+    return list(range(n))
 
 def list_from_range2(n, m):
     """
     This function returns list with n..m (without m) as integers in a list
     """
-    pass # implement me
+    return list(range(n, m))
+
 
 def list_from_range3(n, m):
     """
     This function returns list with n..m (including m(!)) as integers in a list
     """
-    pass # implement me
+    return list(range(n, m+1))
 
 def list_from_range4(n, m):
     """
     This function returns list with n..m (WITHOUT n and including m) as integers in a list
     """
-    pass # implement me
+    return list(range(n+1, m+1))
 
 def list_from_range_by(n, step):
     """
     This function returns list with 0..n as integers by step in a list
     (read the test)
     """
-    pass # implement me
+    return list(range(0,n,step))
 
 def rev_list(lst):
     """
     This function returns list which is a reverse of the argument list
     (read the test)
     """
-    pass # implement me
-  
+    return lst[::-1]
+
 def concat_list_indexwise(lst1, lst2):
     """
     Write a program to add two lists index-wise. 
-    Create a new list that contains the 0th index item from both the list, 
+    Create a new list that contains the 0th index item from both of the lists, 
     then the 1st index item, and so on till the last element. 
     Any leftover items will get added at the end of the new list.
     """
-    parseString # implement me
-
+    return [lst1[i] + lst2[i] for i in range(len(lst1))]
+    
 def square_each_item(lst):
     """
     This function returns list which each item in argument list has been squared
     (read the test)
     """
-    pass # implement me
+    return [x**2 for x in lst]
 
 def remove_empty_strs(lst):
-     """
-     Remove empty strings from the list of strings
-     """
-     pass
+    """
+    Remove empty strings from the list of strings
+    """
+    return [x for x in lst if x != '']
 
 
 def remove_item_from(lst, aaa):
     """
     Remove all occurrences of a specific item from a list.
     """
-    pass
+    return [x for x in lst if x != aaa]
 
 def leave_item_in(lst, aaa):
     """
     Leave all occurrences of a specific item in a list.
     """
-    pass
+    return [x for x in lst if x == aaa]
+        
 
 def length_of(lst):
     """
     return the length of the list
     """
-    pass
+    return len(lst)
